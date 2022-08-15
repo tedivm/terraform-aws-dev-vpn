6c5bc85f7143424038fd825ac46018017340d369
Robert Hafner <tedivm@tedivm.com>
2022-08-15 11:09:29 -0500

    Ability to specify additional security groups

--- network.tf
+++ network.tf
@@ -4,7 +4,7 @@ resource "aws_ec2_client_vpn_network_association" "vpn_subnets" {
 
   client_vpn_endpoint_id = aws_ec2_client_vpn_endpoint.vpn.id
   subnet_id              = var.subnet_ids[count.index]
-  security_groups        = [aws_security_group.vpn_access.id]
+  security_groups        = concat([aws_security_group.vpn_access.id], var.security_group_ids)
 
   lifecycle {
     # This is a bug workaround- https://github.com/hashicorp/terraform-provider-aws/issues/14717
